7330d028a327083701d437f19b6311a5a56bfb11
thibaultconte <thibault1.conte@epitech.eu>
2023-10-25 17:30:24 +0200

    [:hammer:] Optimisation de mes applications spark avec gestion du dernière offset traiter et stocké dans cassandra

--- Big-data-project/spark/bitcoin_rate_stats.py
+++ Big-data-project/spark/bitcoin_rate_stats.py
@@ -1,9 +1,6 @@
-from pyspark import SparkContext
 from pyspark.sql import SparkSession
-from pyspark.streaming import StreamingContext
-from pyspark.sql.functions import col, unix_timestamp, expr, current_timestamp
-from pyspark.sql.types import StructType, StructField, StringType, FloatType, TimestampType
-from pyspark.sql.window import Window
+from pyspark.sql.functions import col, unix_timestamp, expr, current_timestamp, round
+from pyspark.sql.types import DoubleType, TimestampType
 from pyspark.sql import functions as F
 
 spark = SparkSession.builder \
@@ -11,8 +8,6 @@ spark = SparkSession.builder \
     .master("spark://86.201.248.56:7077") \
     .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.2,com.datastax.spark:spark-cassandra-connector_2.12:3.4.1") \
     .getOrCreate()
-
-schema = StructType([StructField("key", StringType(), True), StructField("value", FloatType(), True)])
 
 df = spark \
     .readStream \
@@ -26,7 +21,7 @@ df = spark \
 df = df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)", "CAST(timestamp AS TIMESTAMP)")
 
 # Pré-Traitement des collumns pour utilisation (Conversion de type etc...)  
-df = df.withColumn("value", df["value"].cast(FloatType()))
+df = df.withColumn("value", round(df["value"].cast(DoubleType()), 2))
 df = df.withColumn("timestamp", df["timestamp"].cast(TimestampType()))
 df = df.withColumn("timestamp", unix_timestamp(df["timestamp"], "yyyy-MM-dd HH:mm:ss.SSS").cast("timestamp"))
 
@@ -51,12 +46,10 @@ result_df = aggregated_df.withColumn("variation_rate",
 result_df = result_df.withColumn("win_date_start", expr("window.start"))
 result_df = result_df.withColumn("win_date_end", expr("window.end"))
 
-# Sélection des colonnes souhaitées pour l'insertion dans Cassandra
 columns_to_insert = ["key", "win_date_start", "win_date_end", "start_value", "latest_value", "calculation_time", "difference_rate", "variation_rate"]
 
 result_df = result_df.select(columns_to_insert)
 
-# Fonction pour insérer ou mettre à jour les données dans Cassandra
 def updateCassandra(batch_df, batch_id):
 
     selected_df = batch_df.select("win_date_start", "win_date_end", "start_value", "latest_value", "calculation_time", "difference_rate", "variation_rate")
@@ -72,7 +65,7 @@ def updateCassandra(batch_df, batch_id):
         .options(table="bitcoin_stats", keyspace="bitcoin_data") \
         .save()
 
-# Ecriture sur console de la difference de variation en temps réel
+
 query = result_df.writeStream \
     .outputMode("update") \
     .foreachBatch(updateCassandra) \
